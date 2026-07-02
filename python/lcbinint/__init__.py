4bcdcceb9b31bdbf2c8ccee16e6d2946ab1570d8
NunotaKansuke <kansuke.nunota2997@gmail.com>
2026-07-02 13:35:35 +0900

    Hide adaptive inverse-ray options from public API

--- python/lcbinint/__init__.py
+++ python/lcbinint/__init__.py
@@ -2,6 +2,14 @@ from ._lcbinint import *          # noqa: F401, F403
 from ._lcbinint import lc, obs, bayes, optimize, sample
 from .sampler import SamplerOptions, run_sampler, load_chain, Reparameterization
 
+Options = lc.Options
+Parameters = lc.Parameters
+LimbDarkening = lc.LimbDarkening
+LightCurve = lc.LightCurve
+Effects = lc.Effects
+OrbitalMotionMode = lc.OrbitalMotionMode
+XallarapParamType = lc.XallarapParamType
+
 # Build the Python-extended Model subclass and replace bayes.Model
 from .model import _build_model_class as _bmc
 bayes.Model = _bmc(bayes._Model)
@@ -9,5 +17,7 @@ del _bmc
 
 __all__ = [
     "lc", "obs", "bayes", "optimize", "sample",
+    "Options", "Parameters", "LimbDarkening", "LightCurve", "Effects",
+    "OrbitalMotionMode", "XallarapParamType",
     "SamplerOptions", "run_sampler", "load_chain", "Reparameterization",
 ]
